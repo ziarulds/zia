581c50eb410954c90bd1c6a1cab62badf541d9a3
ziarul islam <mdziarulislam.bu.7@gmail.com>
2025-01-14 21:34:01 +0600

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-class student:
-    def __init__(self,fname,tuition):
-        self.first_name=fname
-        self.tuition=tuition
-
-stu = student('ziarul',8000)   
-print('name=',stu.first_name)  
-print('tuitio_fee=',stu.tuition) 
-        
